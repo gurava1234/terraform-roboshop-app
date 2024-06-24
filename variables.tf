0a35611389eff9708aaada9691ea3d40b925700e
gurava1234 <bachuguravareddy47@gmail.com>
2024-06-24 08:42:46 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "common_tags" {
+    # default = {
+    #     project = "roboshop"
+    #     environment = "dev"
+    #     terraform = "true"
+    # }
+}
+
+variable "tags" {
+}
+
+variable "project_name" {
+    #default = "roboshop"
+}
+
+variable "environment" {
+    #default = "dev"
+}
+variable "zone_name" {
+    #default = "daws76s.cfd"
+}
+variable "vpc_id" {
+
+}
+variable "component_sg_id" {
+
+}
+variable "private_subnet_ids" {
+
+}
+variable "iam_instance_profile" {
+
+}
+variable "app_alb_listener_arn" {
+
+}
+variable "rule_priority" {
+    
+}
+variable "app_version" {
+    
+}
